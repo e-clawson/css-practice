6475fcef09c03005d8d56b74821c9f8450d71b6d
Elizabeth Clawson <emclawson1@gmail.com>
2025-08-22 15:41:40 +0530

    added text

--- src/App.jsx
+++ src/App.jsx
@@ -13,14 +13,16 @@ function App() {
       <a href="https://www.w3schools.com/css/css3_flexbox.asp">W3 Schools</a>
       <a href="https://css-tricks.com/snippets/css/a-guide-to-flexbox/">CSS Tricks</a>
       <p>Text About CSS flexbox </p>
-      <p>To align </p>
+      <p>To use CSS Flexbox, you need to includethe following: </p>
+      <p> display: flex;</p>
     </div>
     <div className='text'>
       <h2>justify-content</h2>
       <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/justify-content">MDN Docs</a>
       <a href="https://www.w3schools.com/cssref/css3_pr_justify-content.php">W3 Schools</a>
       <p>The Justify Content property aligns a flexible container's items horizontally (to align things vertically, one must use the align-items property instead) </p>
-      <p>To align </p>
+      <p>To align items to the center, you use the following: </p>
+      <p>align-items: center;</p>
     </div>
     <div className='text-boxes'>
         <p className='text-box'>Box 1</p>
