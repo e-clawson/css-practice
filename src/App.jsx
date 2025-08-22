e52e09a3866160dd927e52b87a1d4adbc6e73288
Elizabeth Clawson <emclawson1@gmail.com>
2025-08-22 14:28:03 +0530

    added text boxes

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,20 @@ function App() {
   return (
     <>
       <h1>CSS Practice!</h1>
-      
+      <div className='text boxes'>
+        <p className='text box'>Box 1</p>
+        <p className='text box'>Box 2</p>
+        <p className='text box'>Box 3</p>
+        <p className='text box'>Box 4</p>
+        <p className='text box'>Box 5</p>
+        <p className='text box'>Box 6</p>
+        <p className='text box'>Box 7</p>
+        <p className='text box'>Box 8</p>
+        <p className='text box'>Box 9</p>
+        <p className='text box'>Box 10</p>
+        <p className='text box'>Box 11</p>
+        <p className='text box'>Box 12</p>
+      </div>
     </>
   )
 }
